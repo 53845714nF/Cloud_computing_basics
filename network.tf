a02a73552f2737ab9752a69e2ff5b44ed576e14c
53845714nF <seba98feustel@gmail.com>
2022-10-26 20:58:31 +0200

    First commit

---
+++ network.tf
@@ -0,0 +1,51 @@
+# Virtuelle Private Cloud
+resource "aws_vpc" "todo_vpc" {
+  cidr_block = "172.16.0.0/16"
+}
+
+# Gateway ins Internet
+resource "aws_internet_gateway" "todo_gw" {
+   vpc_id = aws_vpc.todo_vpc.id
+}
+
+# Routing Tabelle 
+resource "aws_route_table" "todo_route_table" {
+   vpc_id = aws_vpc.todo_vpc.id
+
+   route {
+     cidr_block = "0.0.0.0/0"
+     gateway_id = aws_internet_gateway.todo_gw.id
+   }
+   route {
+     ipv6_cidr_block = "::/0"
+     gateway_id      = aws_internet_gateway.todo_gw.id
+   }
+}
+
+# Eigenes subnetz
+resource "aws_subnet" "todo_subnet" {
+  vpc_id            = aws_vpc.todo_vpc.id
+  cidr_block        = "172.16.10.0/24"
+  availability_zone = "us-east-1a"
+}
+
+#Zuweisen von subnet mit Route Table
+resource "aws_route_table_association" "a" {
+   subnet_id      = aws_subnet.todo_subnet.id
+   route_table_id = aws_route_table.todo_route_table.id
+ }
+
+# Estellen von Netzwerkinterface
+resource "aws_network_interface" "todo_interface" {
+  subnet_id   = aws_subnet.todo_subnet.id
+  private_ips = ["172.16.10.100"]
+  security_groups = [aws_security_group.allow_web.id]
+}
+
+# Elastic IP erstellen und dem Networkinterface hinzufügen
+resource "aws_eip" "todo_ip" {
+   vpc                       = true
+   network_interface         = aws_network_interface.todo_interface.id
+   associate_with_private_ip = "172.16.10.100"
+   depends_on                = [aws_internet_gateway.todo_gw]
+ } 
---
+++ s3.tf
@@ -0,0 +1,126 @@
+# Erstelen von S3 Bucket
+resource "aws_s3_bucket" "todo_bucket" {
+  bucket = "todo-s3-img"
+}
+
+# Rechte für S3 Bucket
+resource "aws_s3_bucket_acl" "todo_bucket_acl" {
+  bucket = aws_s3_bucket.todo_bucket.id
+  acl    = "public-read"
+}
+
+# Bild hochladen
+resource "aws_s3_object" "img_upload" {
+  bucket = "todo-s3-img"
+  key = "Bild Logo"
+  source = "content/index.png"
+  acl = "public-read"
+  depends_on = [aws_s3_bucket.todo_bucket]
+}
+
+# Html hochladen 
+resource "aws_s3_object" "html_upload" {
+  bucket = "todo-s3-img"
+  key = "index.html"
+  source = "content/index.html"
+  acl = "public-read"
+  depends_on = [aws_s3_bucket.todo_bucket]
+}
+
+locals {
+  s3_origin_id = "myS3Origin"
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.todo_bucket.bucket_regional_domain_name
+    origin_id   = local.s3_origin_id
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "Some comment"
+  default_root_object = "index.html"
+
+  
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  # Cache behavior with precedence 0
+  ordered_cache_behavior {
+    path_pattern     = "/content/immutable/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+      headers      = ["Origin"]
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  # Cache behavior with precedence 1
+  ordered_cache_behavior {
+    path_pattern     = "/content/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  price_class = "PriceClass_200"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["US", "CA", "GB", "DE"]
+    }
+  }
+
+  tags = {
+    Environment = "production"
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
